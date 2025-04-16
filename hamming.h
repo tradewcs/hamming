651f3c08bcbc7845084308a2a502298012e4ceaa
Tradewcs <tradewcs@gmail.com>
2025-04-16 03:56:53 +0300

    stucturized code

--- hamming.h
+++ hamming.h
@@ -34,6 +34,33 @@ using namespace std;
 // sum = 1
 
 namespace Hamming {
+    int getParityBitsCount(int bitsCount) {
+        int parityBitsCount = 0;
+        while ((1 << parityBitsCount) < bitsCount + parityBitsCount + 1) {
+            parityBitsCount++;
+        }
+
+        return parityBitsCount;
+    }
+
+    int getBitsCountFromParity(int parityBitsCount) {
+        int bitsCount = 0;
+        while (getParityBitsCount(bitsCount) < parityBitsCount) {
+            bitsCount++;
+        }
+
+        return bitsCount;
+    }
+
+    int getDataMinSize(int encodedSize) {
+        int dataSize = 0;
+        while (dataSize + getParityBitsCount(dataSize) < encodedSize) {
+            dataSize++;
+        }
+
+        return dataSize;
+    }
+
     int calculateParity(const std::vector<int> &vec, int bitNumber) {
         int sum = 0;
 
@@ -45,8 +72,80 @@ namespace Hamming {
 
         sum -= vec[bitNumber - 1];
 
-        return sum % 2 != 1;
+        return sum % 2;
     }
 
+    std::vector<int> getHaffmanTemplate(const std::vector<int> &data) {
+        int totalBitsCount = getParityBitsCount(data.size());
+        std::vector<int> template(data.size() + totalBitsCount);
 
+        int i = 0;
+        int j = 0;
+        while (i < totalBitsCount) {
+            if ((i + 1) && !(i + 1 & i)) {
+                template[i] = 0;
+            } else {
+                template[i] = data[j];
+                j++;
+            }
+
+            i++;
+        }
+
+        return template;
+    }
+
+    std::vector<int> encode(const std::vector<int> &data) {
+        int parityBitsCount = gettParityBitsCount(data);
+        vector<int> parityBits(parityBitsCount);
+
+        std::vector<int> encodedData = getHaffmanTemplate(data);
+        for (int i = 0; i < parityBitsCount; i++) {
+            int position = (1 << i) - 1;
+
+            int parity = Hamming::calculateParity(encodedData, position);
+            parityBits[i] = parity;
+            encodedData[position] = parity;
+        }
+
+        return encodedData;
+    }
+
+    std::vector<int> getParityBits(const std::vector<int> &data) {
+        int parityBitsCount = gettParityBitsCount(data);
+        vector<int> parityBits(parityBitsCount);
+
+        std::vector<int> encodedData = getHaffmanTemplate(data);
+        for (int i = 0; i < parityBitsCount; i++) {
+            int position = (1 << i) - 1;
+
+            int parity = Hamming::calculateParity(encodedData, position);
+            parityBits[i] = parity;
+            encodedData[position] = parity;
+        }
+
+        return parityBits;
+    }
+
+    std::vector<int> decode(const std::vector<int> &encodedData) {
+        std::vector<int> parityBits(getParityBitsCount(getDataMinSize(encodedData.size())));
+        int parityBitsCount = parityBits.size();
+
+        std::vector<int> decodedData(encodedData);
+        int errorSum = 0;
+        for (int i = 0; i < parityBitsCount; i++) {
+            int position = (1 << i) - 1;
+            parityBits[i] = Hamming::calculateParity(encodedBIts, position);
+
+            if (parityBits[i] != encodedData[position]) {
+                errorSum += position + 1;
+            }
+        }
+
+        if (errorSum > 0) {
+            decodedData[errorSum] ^= 1 ;
+        }
+
+        return decodedData;
+    }
 };
--- main.cpp
+++ main.cpp
@@ -5,122 +5,20 @@
 
 using namespace std;
 
+vector<int> getInput(int inputSize) {
+    vecor<int> input(inputSize);
+    for(int i = 0; i < inputSize; i++) {
+        cin >> input[i];
+    }
+
+    return input;
+}
+
+// (15, 11) hamming code
 int main() {
-
-    // cout << Humming::calculateParity(bits, 3) << endl;
-
-    int bitsCount;
-    // cout << "Enter number of bits -> ";
-    // cin >> bitsCount;
-
-    // vector<int> input = Utils::parseString("10100101001");
-    // vector<int> input = Utils::parseString("11011010010011010011001011111");
-    vector<int> input = Utils::parseString("01010001");
-    // vector<int> input = Utils::parseString("11011010010011010011001011111001000000010100001110");
-    
-    bitsCount = input.size();
-
-    cout << "data size:       " << bitsCount << endl;
-    cout << "input data:      ";
-    Utils::printVector(input);
-
-    // for (int i = 0; i < bitsCount; i++) {
-    //     cin >> input[i];
-    // }
-
-
-    int parityBitsCount = 0;
-    while ((1 << parityBitsCount) < bitsCount + parityBitsCount + 1) {
-        parityBitsCount++;
-    }
-
-    // vector<int> bits(totalBitsCount);
-    int totalBitsCount = bitsCount + parityBitsCount;
-    vector<int> bits(totalBitsCount);
-
-    int i = 0;
-    int j = 0;
-    while (i < totalBitsCount) {
-        if ((i + 1) && !(i + 1 & i)) {
-            bits[i] = 0;
-        } else {
-            bits[i] = input[j];
-            j++;
-        }
-
-        i++;
-    }
-
-    vector<int> parityBits(parityBitsCount);
-    for (int i = 0; i < parityBitsCount; i++) {
-        int position = (1 << i) - 1;
-
-        int parity = Hamming::calculateParity(bits, position);
-        parityBits[i] = parity;
-        bits[position] = parity;
-    }
-
-    cout << "parity bits:     ";
-    Utils::printVector(parityBits);
-    cout << endl;
-
-    cout << "                 ";
-    for (int i = 0; i < totalBitsCount; i++) cout << i << " ";
-    cout << endl;
-
-    cout << "humming encoded: ";
-    Utils::printVector(bits);
-
-    cout << "                 ";
-    for (int i = 0; i < totalBitsCount; i++) {
-        // if ((i + 1) && !(i + 1 & i)) {
-        if (i % 2 == 0010100000101) {
-            cout << "* ";
-        } else {
-            cout << "  ";
-        }
-    }
-    cout << endl;
-
-
-    cout << "endoded in bwsr: ";
-    cout << "0 1 0 1 1 0 0 0 1 1 1 0" << endl;
-
-    // cout << "endoded in bwsr: ";
-    // cout << "0 1 0 1 0 0 0 0 0 1 0 1" << endl;
-
-
-
-    return 0;
-
-    int errorSum = 0;
-    for (int i = 0; i < parityBitsCount; i++) {
-        int position = 1 << i;
-        parityBits[i] = Hamming::calculateParity(bits, position) % 2;
-
-        if (parityBits[i] == 1) {
-            errorSum += position;
-        }
-    }
-
-    if (errorSum > 0) {
-        bits[errorSum - 1] ^= 1 ;
-    }
-
-    cout << "corrected data:  ";
-    Utils::printVector(bits);
-
-
-
-
-
-    cout << "actual message:  ";
-    for (int i = 0; i < totalBitsCount; i++) {
-        if ((i + 1) && !(i + 1 & i)) continue;
-
-        cout << bits[i] << "";
-    }
-    cout << endl;
+    vector<int> input = getInput(11);
+    int parityBitsCount = countParityBitsCount(bitsCount);
+    vector<int> encodedBIts = Hamming::encode(input);
 
     return 0;
 }
